c53ad5f24661b4646519fb94e4713ba0ffaf3def
Vitaliy Cholan <vitaliy.cholan.31@gmail.com>
2024-08-10 12:39:20 +0300

    Add basic markup of converter

--- cryptocurrency-converter/src/app/app.component.ts
+++ cryptocurrency-converter/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './component/header/header.component';
+import { ConverterComponent } from './component/converter/converter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ConverterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
