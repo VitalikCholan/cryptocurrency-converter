b1d3a22150e64e46a9e6be28a14d68f1ccce0694
Vitaliy Cholan <vitaliy.cholan.31@gmail.com>
2024-08-18 14:41:29 +0300

    setup interaction between quantity input and price input

--- cryptocurrency-converter/src/app/component/converter/converter.component.ts
+++ cryptocurrency-converter/src/app/component/converter/converter.component.ts
@@ -17,6 +17,9 @@ type Quote = {
 export class ConverterComponent implements OnInit {
   cryptoData: { name: string; symbol: string; quote: Quote }[] = [];
   selectedCryptos: string[] = ['bitcoin', 'ethereum', 'litecoin'];
+  selectedSymbol: string = '';
+  cryptoQuantity: number = 1;
+  calculatedPrice: number = 0;
 
   constructor(private coinMarketCapService: CoinMarketCapService) {}
 
@@ -25,6 +28,10 @@ export class ConverterComponent implements OnInit {
       (response: any) => {
         this.cryptoData = Object.values(response.data);
         console.log(this.cryptoData);
+        if (this.cryptoData.length > 0) {
+          this.selectedSymbol = this.cryptoData[0].symbol;
+          this.updatePrice(); // Calculate initial price
+        }
       },
       (error) => {
         console.error('There was an error fetching the data!', error);
@@ -32,18 +39,40 @@ export class ConverterComponent implements OnInit {
     );
   }
 
-  selectedPrice: number = 0;
-
   onCryptoChange(event: Event): void {
     const selectElement = event.target as HTMLSelectElement;
-    const selectedSymbol = selectElement.value;
+    this.selectedSymbol = selectElement.value;
+    this.updatePrice();
+  }
 
+  onQuantityChange(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    this.cryptoQuantity = +inputElement.value; // Convert value to number
+    this.updatePrice();
+  }
+
+  onPriceChange(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    const newPrice = +inputElement.value;
+    if (this.selectedSymbol) {
+      const selectedCrypto = this.cryptoData.find(
+        (crypto) => crypto.symbol === this.selectedSymbol
+      );
+      if (selectedCrypto) {
+        const pricePerUnit = selectedCrypto.quote.USD.price || 0;
+        this.cryptoQuantity = newPrice / pricePerUnit;
+      }
+    }
+  }
+
+  updatePrice(): void {
     const selectedCrypto = this.cryptoData.find(
-      (crypto) => crypto.symbol === selectedSymbol
+      (crypto) => crypto.symbol === this.selectedSymbol
     );
 
     if (selectedCrypto) {
-      this.selectedPrice = selectedCrypto.quote.USD.price || 0;
+      const price = selectedCrypto.quote.USD.price || 0;
+      this.calculatedPrice = price * this.cryptoQuantity;
     }
   }
 }
