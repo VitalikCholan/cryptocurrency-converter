7a441994538ae0484f0f13b5860b6b4f96a30e58
Vitaliy Cholan <vitaliy.cholan.31@gmail.com>
2024-09-01 16:12:08 +0300

    Change API and approach to display crypto from new API

--- cryptocurrency-converter/src/app/component/service/coin-market-cap.service.ts
+++ cryptocurrency-converter/src/app/component/service/coin-market-cap.service.ts
@@ -1,23 +1,29 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CoinMarketCapService {
-  private apiUrl: string = 'v2/cryptocurrency/quotes/latest';
+  private apiUrl: string = '/v1/cryptocurrency/listings/latest';
   private apiKey: string = 'dbaeffee-252c-4d88-b04e-e70106198aa8';
 
   constructor(private http: HttpClient) {}
 
-  getCryptoData(slugs: string[]): Observable<unknown> {
+  getCryptoData(params?: any): Observable<any> {
     const headers = new HttpHeaders({
       'X-CMC_PRO_API_KEY': this.apiKey,
     });
-    const slugQuery = slugs.length ? slugs.join(',') : 'bitcoin';
-    return this.http.get<unknown>(`${this.apiUrl}?slug=${slugQuery}`, {
-      headers,
-    });
+
+    let httpParams = new HttpParams();
+
+    if (params) {
+      Object.keys(params).forEach((key) => {
+        httpParams = httpParams.set(key, params[key]);
+      });
+    }
+
+    return this.http.get<any>(this.apiUrl, { headers, params: httpParams });
   }
 }
